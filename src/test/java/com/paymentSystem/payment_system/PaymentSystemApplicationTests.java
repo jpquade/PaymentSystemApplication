fd66775876ca2ef28dc8c5df59f5e4dc9b9ffdd9
jquade <jpquade@gmail.com>
2025-06-14 16:21:58 -0700

    Initial commit

--- src/test/java/com/paymentSystem/payment_system/PaymentSystemApplicationTests.java
+++ src/test/java/com/paymentSystem/payment_system/PaymentSystemApplicationTests.java
@@ -8,6 +8,9 @@ class PaymentSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test checks if the Spring application context loads successfully.
+		// If there are any issues with the configuration or beans, this test will fail.
+		// No additional logic is needed here as we are just verifying the context loading.
 	}
 
 }
